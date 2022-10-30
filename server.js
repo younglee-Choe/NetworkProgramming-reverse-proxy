dcb991f1d74ffff4eea80df287bdff7571abbfa2
younglee-Choe <leele7111@naver.com>
2022-10-30 15:49:58 +0900

    https server

--- server.js
+++ server.js
@@ -15,10 +15,33 @@ app.get('*', (req, res) => {
 })
 
 const options = {
-    key: fs.readFileSync('./leele-react/private.pem'),
-    cert: fs.readFileSync('./leele-react/public.pem')
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
 }
 
 https.createServer(options, app).listen(port)
 
 console.log(`Server is listening on ${port}`)
+
+
+// express
+// const express = require("express")
+// const app = express()
+// const port = process.env.PORT || 5000
+
+// app.use(express.static(__dirname + '/leele-react/build'))
+// app.set('views', __dirname + '/leele-react/build')
+// app.engine('html', require('ejs').renderFile)
+// app.set('view engine', 'html')
+
+// app.get('/test', (req, res) => {
+//     res.json([{ id: 1, username: "cyl" }])
+// })
+
+// app.get('*', (req, res) => {
+//     res.render('./leele-react/build/index.html')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Express is listening on ${port}`)
+// })
