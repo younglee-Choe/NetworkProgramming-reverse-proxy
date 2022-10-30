fcff298143beea69ad48961270bff68adef3ceec
younglee-Choe <leele7111@naver.com>
2022-10-30 18:53:03 +0900

    https express server

--- leele-server/server.js
+++ leele-server/server.js
@@ -1,35 +1,25 @@
 const express = require("express")
-const routes = require('../leele-react/src/router/routes.js')
+const https = require('https')
+const fs = require('fs')
+const ejs = require('ejs')
+const path = require('path')
+const router = require('../leele-react/src/router/routes')
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = 4041
 
-app.use(routes)
+app.use(express.static(path.join(__dirname, '../leele-react/build')))
+app.set('views', __dirname + '../leele-react/build')
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 
-app.listen(port, () => {
-    console.log(`Express is listening on ${port}`)
-})
+app.use(router)
 
-// const https = require('https')
-// const fs = require('fs')
-// const path = require('path')
+const options = {
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
+}
 
-// const options = {
-//     key: fs.readFileSync('../leele-react/private.pem'),
-//     cert: fs.readFileSync('../leele-react/public.pem')
-// }
+https.createServer(options, app).listen(port)
 
-// https.createServer(options, function(req, res) {
-//     // fs.readFile(path.join(__dirname, '../leele-react/build/index.html'))
-//     fs.readFile('../leele-react/build/index.html', function(error, data) {
-//         if(error){
-//             console.log(error)
-//         }
-//         else{
-//             res.writeHead(200)
-//             res.end(data)
-//         }
-//     })
-// }).listen(port)
-
-// console.log(`Server is listening on ${port}`)
+console.log(`Server is listening on ${port}`)
