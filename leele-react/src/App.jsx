fd80c45eb697d844d31d30803d9df6b52869d4e9
younglee-Choe <leele7111@naver.com>
2022-11-02 10:47:53 +0900

    client-side rendering in React

--- leele-react/src/App.jsx
+++ leele-react/src/App.jsx
@@ -1,16 +1,27 @@
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import Home from './Home'
+import axios from 'axios'
+import Header from './Heder'
 import Basic from './ar/Basic'
 import Cube from './ar/Cube'
+import VRScene from './ar/VRScene'
 
 function App() {
+
+  useEffect(() => {
+    axios.get('/api/test')
+      .then(res => console.log(res))
+      .catch()
+  })
+
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/basic" exact element={<Basic />} />
+          <Route path="/" exact element={<Basic />} />
+          <Route path="/cube" exact element={<Cube />} />
+          <Route path="/location" exact element={<VRScene />} />
         </Routes>
       </BrowserRouter>
     </div>
